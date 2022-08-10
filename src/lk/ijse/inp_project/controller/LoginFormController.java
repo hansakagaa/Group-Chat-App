9d0173bc76d19443b09718a4dacd1dc998df0b51
Hasii-boy <hansakagaa@gmail.com>
2022-08-11 00:02:51 +0530

    Update login form

--- src/lk/ijse/inp_project/controller/LoginFormController.java
+++ src/lk/ijse/inp_project/controller/LoginFormController.java
@@ -1,8 +1,14 @@
 package lk.ijse.inp_project.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 /**
  * @author : hansakagaa
@@ -13,5 +19,46 @@ public class LoginFormController {
 
     @FXML
     private void login_on_action(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void playMouseEnterAnimation(MouseEvent event) {
+        if (event.getSource() instanceof JFXButton){
+            JFXButton button = (JFXButton) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), button);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+        }
+        if (event.getSource() instanceof JFXTextField){
+            JFXTextField field = (JFXTextField) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), field);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+        }
+    }
+
+    @FXML
+    public void playMouseExitAnimation(MouseEvent event) {
+        if (event.getSource() instanceof JFXButton){
+            JFXButton button = (JFXButton) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), button);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+            button.setEffect(null);
+        }
+        if (event.getSource() instanceof JFXTextField){
+            JFXTextField field = (JFXTextField) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), field);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+
+            field.setEffect(null);
+        }
     }
 }
