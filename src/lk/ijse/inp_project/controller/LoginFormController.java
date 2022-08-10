86c2cf46b7bec66129e7178b2d5ca108fb5431a8
Hasii-boy <hansakagaa@gmail.com>
2022-08-10 23:45:06 +0530

    Update content login form

--- src/lk/ijse/inp_project/controller/LoginFormController.java
+++ src/lk/ijse/inp_project/controller/LoginFormController.java
@@ -1,7 +1,17 @@
 package lk.ijse.inp_project.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * @author : hansakagaa
  **/
 public class LoginFormController {
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private void login_on_action(ActionEvent actionEvent) {
+    }
 }
