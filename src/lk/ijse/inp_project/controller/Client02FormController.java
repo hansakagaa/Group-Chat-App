022659e2f359141aad83a4594cf4352a2b4417f4
Hasii-boy <hansakagaa@gmail.com>
2022-08-10 16:37:02 +0530

    Add client form

--- src/lk/ijse/inp_project/controller/Client02FormController.java
+++ src/lk/ijse/inp_project/controller/Client02FormController.java
@@ -1,7 +1,38 @@
 package lk.ijse.inp_project.controller;
 
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 /**
  * @author : hansakagaa
  **/
 public class Client02FormController {
+    @FXML
+    private ImageView imgCamera;
+    @FXML
+    private ImageView imgSend;
+    @FXML
+    private JFXTextArea txtArea;
+    @FXML
+    private JFXTextField txtMessage;
+
+
+    public void initialize(){
+    }
+
+    @FXML
+    private void message_key_released(KeyEvent keyEvent) {
+    }
+
+    @FXML
+    private void send_message_on_click(MouseEvent event) {
+    }
+
+    @FXML
+    private void choose_image_on_click(MouseEvent event) {
+    }
 }
