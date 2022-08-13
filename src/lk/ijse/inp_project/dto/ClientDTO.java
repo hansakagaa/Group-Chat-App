af8a3f964082fb0b120ffe112cf545c40929b2a1
Hasii-boy <hansakagaa@gmail.com>
2022-08-14 02:10:30 +0530

    Add clientDTO

---
+++ src/lk/ijse/inp_project/dto/ClientDTO.java
@@ -0,0 +1,10 @@
+package lk.ijse.inp_project.dto;
+
+/**
+ * @author : hansakagaa
+ **/
+public class ClientDTO {
+    public static int portNum;
+    public static String hostName;
+    public static String userName;
+}
