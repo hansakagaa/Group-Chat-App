a2e939637e41071b49ee2a82e732d5d331c6ece7
Hasii-boy <hansakagaa@gmail.com>
2022-08-14 01:52:00 +0530

    Initial server form

--- src/lk/ijse/inp_project/controller/ServerFormController.java
+++ src/lk/ijse/inp_project/controller/ServerFormController.java
@@ -1,7 +1,24 @@
 package lk.ijse.inp_project.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * @author : hansakagaa
  **/
 public class ServerFormController {
+    @FXML
+    public JFXTextArea txtArea;
+    @FXML
+    public JFXTextField txtPort;
+    @FXML
+    public JFXButton btnSubmit;
+
+    @FXML
+    public void port_submit_on_action(ActionEvent actionEvent) {
+
+    }
 }
