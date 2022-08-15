a9f777669f6c955aede7698c03132955a0da4b48
Hasii-boy <hansakagaa@gmail.com>
2022-08-15 13:13:42 +0530

    Add content client

--- src/lk/ijse/inp_project/controller/bo/Client.java
+++ src/lk/ijse/inp_project/controller/bo/Client.java
@@ -1,7 +1,75 @@
 package lk.ijse.inp_project.controller.bo;
 
+import lk.ijse.inp_project.controller.ServerFormController;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author : hansakagaa
  **/
 public class Client {
+    String userName;
+    String message;
+    private final DataOutputStream dataOutputStream;
+    private final DataInputStream dataInputStream;
+
+    public DataOutputStream getDataOutputStream(){
+        return dataOutputStream;
+    }
+
+    Client(String userName, DataInputStream dataInputStream, DataOutputStream dataOutputStream){
+        this.userName = userName;
+        this.dataOutputStream = dataOutputStream;
+        this.dataInputStream = dataInputStream;
+
+        startClient();
+    }
+
+    private void startClient() {
+        new Thread(() ->{
+            try {
+                while(true) {
+                    message = dataInputStream.readUTF();
+                    List<Client> users = Server.clients;
+                    for (Client user : users) {
+                        DataOutputStream dataOutputStream = user.getDataOutputStream();
+                        dataOutputStream.writeUTF(message);
+                    }
+                }
+            } catch (IOException E) {
+                System.out.println(E.getMessage());
+
+                closeAll(dataInputStream, dataOutputStream);
+
+                Server.clients = Server.clients.stream().filter(e -> {
+                    if(!(e == this)) {
+                        ServerFormController.txtArea.appendText("\nDisconnected : " + userName);
+                        try {
+                            e.getDataOutputStream().writeUTF(userName);
+                        } catch (IOException er) {
+                            System.out.println(er.getMessage());
+                        }
+                    }
+                    return !(e == this);
+                }).collect(Collectors.toList());
+            }
+        }).start();
+    }
+
+    private void closeAll(DataInputStream dataInputStream, DataOutputStream dataOutputStream){
+        try {
+            if (dataInputStream != null) {
+                dataInputStream.close();
+            }
+            if (dataOutputStream != null) {
+                dataOutputStream.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
