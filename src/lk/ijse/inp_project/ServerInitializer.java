e7577a8a3f9a23d70280d9d6d5cba98309e1e26b
Hasii-boy <hansakagaa@gmail.com>
2022-08-17 11:48:23 +0530

    Create client handler

--- src/lk/ijse/inp_project/ServerInitializer.java
+++ src/lk/ijse/inp_project/ServerInitializer.java
@@ -1,8 +1,11 @@
 package lk.ijse.inp_project;
 
+import lk.ijse.inp_project.util.ClientHandler;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * @author : hansakagaa
--- src/lk/ijse/inp_project/controller/ServerFormController.java
+++ src/lk/ijse/inp_project/controller/ServerFormController.java
@@ -35,53 +35,6 @@ public class ServerFormController {
     @FXML
     public Label lblUserSize;
 
-//
-    public static List<Client> clients;
-    public static DataOutputStream dataOutputStream;
-    DataInputStream dataInputStream;
-    String userName;
-//
-
-    public void initialize(){
-        System.out.println(5000);
-
-        clients = new ArrayList<>();
-
-        System.out.println("29");
-        try {
-            ServerSocket serverSocket = new ServerSocket(5000);
-            Socket socket;
-            while (!serverSocket.isClosed()){
-                System.out.println("34");
-                socket = serverSocket.accept();
-//        ServerFormController.txtArea.appendText("Start Server..!\n");
-                System.out.println("Start Server..!\n");
-                dataInputStream = new DataInputStream(socket.getInputStream());
-                dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                System.out.println("39");
-                userName = dataInputStream.readUTF();
-                Client client = new Client(userName, dataInputStream, dataOutputStream);
-
-//                ServerFormController.txtArea.appendText("\nConnected : " + userName);
-                System.out.println("\nConnected : " + userName);
-
-                clients.add(client);
-                System.out.println("47");
-                List<Client> users = Server.clients;
-                for (Client user : users) {
-                    DataOutputStream dataOutputStream = user.getDataOutputStream();
-                    dataOutputStream.writeUTF(userName);
-                    System.out.println("52");
-                }
-//                ServerFormController.lblUserSize.setText("Current User : "+ Server.clients.size());
-            }
-        }catch (IOException e){
-            System.out.println(e.getMessage());
-        }
-
-
-    }
-
     @FXML
     private void port_submit_on_action() {
         int PORT = Integer.parseInt(txtPort.getText());
@@ -101,17 +54,5 @@ public class ServerFormController {
         primaryStage.centerOnScreen();
         primaryStage.show();
     }
-//
-    public void setAppendText(String message){
-        txtArea.setText(message);
-    }
-//
-    public void setText(){
-        try {
-            txtArea.setText(Server.message);
-        }catch (NullPointerException e){
-            System.out.println("catch : "+e.getMessage());
-        }
 
-    }
 }
